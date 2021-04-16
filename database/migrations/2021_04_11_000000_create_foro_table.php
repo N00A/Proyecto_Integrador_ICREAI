17e6e586a175181bb7f7737c5c52f4a953e45179
N00A <johnj262000@hotmail.com>
2021-04-15 21:49:41 -0500

    #Add: Se agrega migraciones de la tabla foro

---
+++ database/migrations/2021_04_11_000000_create_foro_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foro', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('contenido',1800);
+            $table->integer('genero_id')->unsigned();
+            $table->foreign('genero_id')->references('id')->on('generos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foro');
+    }
+}
